2e356258e0f9a04ec166fa9cc2a42eeb5095506f
stritti <>
2014-03-28 17:10:34 +0100

    formatted

--- src/html/pagination.php
+++ src/html/pagination.php
@@ -69,164 +69,129 @@ defined('_JEXEC') or die;
  * Renders the pagination footer
  *
  * @param   array  $list  Array containing pagination footer
- *
  * @return  string  HTML markup for the full pagination footer
- *
- * @since   3.0
  */
-function pagination_list_footer($list)
-{
-	$html = "<div class=\"pagination\">\n";
-	$html .= $list['pageslinks'];
-	$html .= "\n<input type=\"hidden\" name=\"" . $list['prefix'] . "limitstart\" value=\"" . $list['limitstart'] . "\" />";
-	$html .= "\n</div>";
+function pagination_list_footer($list) {
+   $html = "<div class=\"pagination\">\n";
+   $html .= $list['pageslinks'];
+   $html .= "\n<input type=\"hidden\" name=\"" . $list['prefix'] . "limitstart\" value=\"" . $list['limitstart'] . "\" />";
+   $html .= "\n</div>";
 
-	return $html;
+   return $html;
 }
 
 /**
  * Renders the pagination list
  *
  * @param   array  $list  Array containing pagination information
- *
  * @return  string  HTML markup for the full pagination object
- *
- * @since   3.0
  */
-function pagination_list_render($list)
-{
-	// Calculate to display range of pages
-	$currentPage = 1;
-	$range = 1;
-	$step = 5;
-	foreach ($list['pages'] as $k => $page)
-	{
-		if (!$page['active'])
-		{
-			$currentPage = $k;
-		}
-	}
-	if ($currentPage >= $step)
-	{
-		if ($currentPage % $step == 0)
-		{
-			$range = ceil($currentPage / $step) + 1;
-		}
-		else
-		{
-			$range = ceil($currentPage / $step);
-		}
-	}
+function pagination_list_render($list) {
+   // Calculate to display range of pages
+   $currentPage = 1;
+   $range = 1;
+   $step = 5;
+   foreach ($list['pages'] as $k => $page) {
+      if (!$page['active']) {
+         $currentPage = $k;
+      }
+   }
+   if ($currentPage >= $step) {
+      if ($currentPage % $step == 0) {
+         $range = ceil($currentPage / $step) + 1;
+      } else {
+         $range = ceil($currentPage / $step);
+      }
+   }
 
-	$html = '<ul class="pagination">';
-	$html .= $list['start']['data'];
-	$html .= $list['previous']['data'];
+   $html = '<ul class="pagination">';
+   $html .= $list['start']['data'];
+   $html .= $list['previous']['data'];
 
-	foreach ($list['pages'] as $k => $page)
-	{
-		if (in_array($k, range($range * $step - ($step + 1), $range * $step)))
-		{
-			if (($k % $step == 0 || $k == $range * $step - ($step + 1)) && $k != $currentPage && $k != $range * $step - $step)
-			{
-				$page['data'] = preg_replace('#(<a.*?>).*?(</a>)#', '$1...$2', $page['data']);
-			}
-		}
+   foreach ($list['pages'] as $k => $page) {
+      if (in_array($k, range($range * $step - ($step + 1), $range * $step))) {
+         if (($k % $step == 0 || $k == $range * $step - ($step + 1)) && $k != $currentPage && $k != $range * $step - $step) {
+            $page['data'] = preg_replace('#(<a.*?>).*?(</a>)#', '$1...$2', $page['data']);
+         }
+      }
 
-		$html .= $page['data'];
-	}
+      $html .= $page['data'];
+   }
 
-	$html .= $list['next']['data'];
-	$html .= $list['end']['data'];
+   $html .= $list['next']['data'];
+   $html .= $list['end']['data'];
 
-	$html .= '</ul>';
-	return $html;
+   $html .= '</ul>';
+   return $html;
 }
 
 /**
  * Renders an active item in the pagination block
  *
  * @param   JPaginationObject  $item  The current pagination object
- *
  * @return  string  HTML markup for active item
- *
- * @since   3.0
  */
-function pagination_item_active(&$item)
-{
-	// Check for "Start" item
-	if ($item->text == JText::_('JLIB_HTML_START'))
-	{
-		$display = '<i class="fi-previous"></i>';
-	}
+function pagination_item_active(&$item) {
+   // Check for "Start" item
+   if ($item->text == JText::_('JLIB_HTML_START')) {
+      $display = '<i class="fi-previous"></i>';
+   }
 
-	// Check for "Prev" item
-	if ($item->text == JText::_('JPREV'))
-	{
-		$display = '<i class="fi-rewinf"></i>';
-	}
+   // Check for "Prev" item
+   if ($item->text == JText::_('JPREV')) {
+      $display = '<i class="fi-rewinf"></i>';
+   }
 
-	// Check for "Next" item
-	if ($item->text == JText::_('JNEXT'))
-	{
-		$display = '<i class="fi-fast-forward"></i>';
-	}
+   // Check for "Next" item
+   if ($item->text == JText::_('JNEXT')) {
+      $display = '<i class="fi-fast-forward"></i>';
+   }
 
-	// Check for "End" item
-	if ($item->text == JText::_('JLIB_HTML_END'))
-	{
-		$display = '<i class="fi-next"></i>';
-	}
+   // Check for "End" item
+   if ($item->text == JText::_('JLIB_HTML_END')) {
+      $display = '<i class="fi-next"></i>';
+   }
 
-	// If the display object isn't set already, just render the item with its text
-	if (!isset($display))
-	{
-		$display = $item->text;
-	}
+   // If the display object isn't set already, just render the item with its text
+   if (!isset($display)) {
+      $display = $item->text;
+   }
 
-	return "<li><a title=\"" . $item->text . "\" href=\"" . $item->link . "\" class=\"pagenav\">" . $display . "</a></li>";
+   return "<li><a title=\"" . $item->text . "\" href=\"" . $item->link . "\" class=\"pagenav\">" . $display . "</a></li>";
 }
 
 /**
  * Renders an inactive item in the pagination block
  *
  * @param   JPaginationObject  $item  The current pagination object
- *
  * @return  string  HTML markup for inactive item
- *
- * @since   3.0
  */
-function pagination_item_inactive(&$item)
-{
-	// Check for "Start" item
-	if ($item->text == JText::_('JLIB_HTML_START'))
-	{
-		return '<li class="unavailable"><a><i class="fi-previous"></i></a></li>';
-	}
+function pagination_item_inactive(&$item) {
+   // Check for "Start" item
+   if ($item->text == JText::_('JLIB_HTML_START')) {
+      return '<li class="unavailable"><a><i class="fi-previous"></i></a></li>';
+   }
 
-	// Check for "Prev" item
-	if ($item->text == JText::_('JPREV'))
-	{
-		return '<li class="unavailable"><a><i class="fi-rewind"></i></a></li>';
-	}
+   // Check for "Prev" item
+   if ($item->text == JText::_('JPREV')) {
+      return '<li class="unavailable"><a><i class="fi-rewind"></i></a></li>';
+   }
 
-	// Check for "Next" item
-	if ($item->text == JText::_('JNEXT'))
-	{
-		return '<li class="unavailable"><a><i class="fi-fast-forward"></i></a></li>';
-	}
+   // Check for "Next" item
+   if ($item->text == JText::_('JNEXT')) {
+      return '<li class="unavailable"><a><i class="fi-fast-forward"></i></a></li>';
+   }
 
-	// Check for "End" item
-	if ($item->text == JText::_('JLIB_HTML_END'))
-	{
-		return '<li class="unavailable"><a><i class="fi-next"></i></a></li>';
-	}
+   // Check for "End" item
+   if ($item->text == JText::_('JLIB_HTML_END')) {
+      return '<li class="unavailable"><a><i class="fi-next"></i></a></li>';
+   }
 
-	// Check if the item is the active page
-	if (isset($item->active) && ($item->active))
-	{
-		return '<li class="current"><a>' . $item->text . '</a></li>';
-	}
+   // Check if the item is the active page
+   if (isset($item->active) && ($item->active)) {
+      return '<li class="current"><a>' . $item->text . '</a></li>';
+   }
 
-	// Doesn't match any other condition, render a normal item
-	return '<li class="unavailable"><a>' . $item->text . '</a></li>';
+   // Doesn't match any other condition, render a normal item
+   return '<li class="unavailable"><a>' . $item->text . '</a></li>';
 }
