a919add20c1d85f0b59371bc7a387bcce4137c76
stritti <stephan@st-strittmatter.name>
2014-04-01 10:36:23 +0200

    replaced by sass-files

---
+++ Gruntfile.js
@@ -0,0 +1,44 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    sass: {
+      options: {
+        includePaths: ['bower_components/foundation/scss']
+      },
+      dist: {
+        options: {
+         // outputStyle: 'compressed'
+        },
+        files: {
+          'src/css/app.css': 'src/sass/app.scss',
+          'src/css/editor.css': 'src/sass/editor.scss',
+          'src/css/print.css': 'src/sass/print.scss'
+        }        
+      }
+    },
+
+    watch: {
+      grunt: { 
+         files: ['Gruntfile.js']
+      },
+      sass: {
+        files: ['src/sass/**/*.{scss,sass}'],
+        tasks: ['sass']
+      },
+      livereload: {
+         files: ['src/*.html', 'src/*.php', 'src/js/**/*.{js,json}', 'src/css/*.css','src/img/**/*.{png,jpg,jpeg,gif,webp,svg}'],
+         options: {
+            livereload: true
+         }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('build', ['sass']);
+  grunt.registerTask('default', ['build','watch']);
+}
