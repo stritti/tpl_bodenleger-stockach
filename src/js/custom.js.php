83d4bf145970b9a1dfefb9530cba00233d8fd88e
stritti <stephan@st-strittmatter.name>
2014-04-02 15:47:19 +0200

    random ordering of background slides

--- src/js/custom.js.php
+++ src/js/custom.js.php
@@ -22,16 +22,40 @@ header("Content-type:text/javascript; charset=UTF-8");
 header("Cache-Control:must-revalidate");
 ?>
 
+/**
+ * Shuffle an array.
+ * see http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+ */
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+var backgroundSlides = [
+   {url: "/templates/bodenleger-stockach/images/1.jpg"},
+   {url: "/templates/bodenleger-stockach/images/2.jpg"},
+   {url: "/templates/bodenleger-stockach/images/3.jpg"},
+   {url: "/templates/bodenleger-stockach/images/4.jpg"},
+   {url: "/templates/bodenleger-stockach/images/5.jpg"},
+   {url: "/templates/bodenleger-stockach/images/6.jpg"}];
+backgroundSlides= shuffle(backgroundSlides);
+
 $(function() {
    $.sublime_slideshow({
-      src: [
-         {url: "/templates/bodenleger-stockach/images/1.jpg"},
-         {url: "/templates/bodenleger-stockach/images/2.jpg"},
-         {url: "/templates/bodenleger-stockach/images/3.jpg"},
-         {url: "/templates/bodenleger-stockach/images/4.jpg"},
-         {url: "/templates/bodenleger-stockach/images/5.jpg"},
-         {url: "/templates/bodenleger-stockach/images/6.jpg"}
-      ],
+      src: backgroundSlides,
       duration: 12,
       fade: 4,
       scaling: false,
